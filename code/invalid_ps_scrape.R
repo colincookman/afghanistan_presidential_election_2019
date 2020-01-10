3ab927b81d9b6eb2b92be6b8671e6717a1680850
colincookman <ccookman@gmail.com>
2020-01-10 17:36:10 -0500

    Corrections based on scrape errors in invalidated and closd polling stations list

--- code/invalid_ps_scrape.R
+++ code/invalid_ps_scrape.R
@@ -36,7 +36,9 @@ province_list <- xml2::read_html(driver$client$getPageSource()[[1]]) %>%
     rvest::html_children() %>%
     rvest::html_text() %>%
     dplyr::data_frame(province = .)
-  
+
+Sys.sleep(3)
+
 province_list <- province_list %>%
     dplyr::mutate(list_position = 1:length(province_list$province),
                   x = stringr::str_c("#province > option:nth-child(", list_position, ")")
@@ -52,24 +54,91 @@ for(i in (1:length(province_list$x))){
     
     # wait to make sure the page has loaded properly
     Sys.sleep(10)
+
+    # check if there are multiple pages of results data per PC due to a large number of contesting candidates
+    pc_link_list <- driver$client$getPageSource()[[1]] %>% read_html() %>% html_nodes("a") %>% html_attr("href")
+    pagination_list <- pc_link_list[grepl("ajax", pc_link_list)]
     
-    scan_links <- driver$client$getPageSource()[[1]] %>% read_html() %>% 
-    html_nodes("a") %>% html_attr("href")
+    page_count <- length(pagination_list)
+
+    if(page_count > 0) {
       
-    # get a list of all PS scans on the PC page
-    scan_links <- scan_links[grepl(".png|.jpg", scan_links)]
-    
-    ps_scan_links <- c(ps_scan_links, scan_links)
-    
-    Sys.sleep(5)
-    
-    page_source <- driver$client$getPageSource()
-    province_html <- page_source[[1]] %>% read_html()
-    province_data_list_item <- province_html %>% html_nodes(".table-condensed") %>% html_text()
+      # get the first page of data 
+      page_list <- 1:(length(pagination_list))
+        
+      scan_links <- driver$client$getPageSource()[[1]] %>% read_html() %>% 
+        html_nodes("a") %>% html_attr("href")
       
-    # add html data to a master list      
-    province_data_list <- c(province_data_list, province_data_list_item)
+      # get a list of all PS scans on the PC page
+      scan_links <- scan_links[grepl(".png|.jpg", scan_links)]
+      ps_scan_links <- c(ps_scan_links, scan_links)
+    
+      Sys.sleep(5)
       
+      page_source <- driver$client$getPageSource()
+      province_html <- page_source[[1]] %>% read_html()
+      province_data_list_item <- province_html %>% html_nodes(".table-condensed") %>% html_text()
+      
+      # add html data to a master list      
+      province_data_list <- c(province_data_list, province_data_list_item)
+      
+      for(m in (2:(length(page_list)))){
+          
+          page_target <- paste0(".page-link[data-ci-pagination-page^=", '"', page_list[m], '"', ']')
+          Sys.sleep(.75)
+
+          # make sure the next page in the sequence is there first
+          next_page <- NULL
+          while(is.null(next_page)){
+            next_page <- tryCatch({driver$client$findElement(using = 'css', value = page_target)},
+            error = function(e){Sys.sleep(.5)})
+          }
+          
+          # click the next page's button and wait
+          next_page <- driver$client$findElement(using = 'css', value = page_target)
+          next_page$clickElement()
+
+          Sys.sleep(5)
+          
+          # once everything is now loaded properly, get the new page of html data
+          scan_links <- driver$client$getPageSource()[[1]] %>% read_html() %>% 
+            html_nodes("a") %>% html_attr("href")
+      
+          # get a list of all PS scans on the PC page
+          scan_links <- scan_links[grepl(".png|.jpg", scan_links)]
+          ps_scan_links <- c(ps_scan_links, scan_links)
+    
+          Sys.sleep(5)
+      
+          page_source <- driver$client$getPageSource()
+          province_html <- page_source[[1]] %>% read_html()
+          province_data_list_item <- province_html %>% html_nodes(".table-condensed") %>% html_text()
+      
+          # add html data to a master list      
+          province_data_list <- c(province_data_list, province_data_list_item)
+      }
+      
+    } else {
+          
+    # if there is only one page for the PC, just extract the first page of PC html data and move on
+          scan_links <- driver$client$getPageSource()[[1]] %>% read_html() %>% 
+        html_nodes("a") %>% html_attr("href")
+      
+      # get a list of all PS scans on the PC page
+      scan_links <- scan_links[grepl(".png|.jpg", scan_links)]
+      ps_scan_links <- c(ps_scan_links, scan_links)
+    
+      Sys.sleep(5)
+      
+      page_source <- driver$client$getPageSource()
+      province_html <- page_source[[1]] %>% read_html()
+      province_data_list_item <- province_html %>% html_nodes(".table-condensed") %>% html_text()
+      
+      # add html data to a master list      
+      province_data_list <- c(province_data_list, province_data_list_item)
+
+    }
+    
     Sys.sleep(5)
     
 }
@@ -108,8 +177,6 @@ for(a in (1:length(province_data_list))){
   
 
 
-table_out <- table_out %>% filter(!is.na(serial_number))
-
 write.csv(table_out, "./audit_data/invalidated_ps_data.csv", row.names = F)
 
 
@@ -133,6 +200,7 @@ ps_scan_links_write = data.frame(lapply(ps_scan_links_out, as.character), string
 
 write.csv(ps_scan_links_write, "./audit_data/invalidated_ps_scan_links.csv", row.names = F)
 
+# DOWNLOAD
 scan_links_list <- read_csv("./audit_data/invalidated_ps_scan_links.csv")
 
 # create a subdirectory for scraped polling station scans if it doesn't already exist
@@ -197,7 +265,7 @@ write.csv(missing_download_targets, "./audit_data/invalidated_ps_scans_broken_li
 rm(list = ls())
 
 prelim_af_candidate_ps_data_2019_lite <- read_csv("results_data/first_round_preliminary_results/prelim_af_candidate_ps_data_2019_lite.csv")
-invalidated_ps <- read_csv("./audit_data/invalidated_ps_data.csv")
+invalidated_ps_data <- read_csv("./audit_data/invalidated_ps_data.csv")
 ps_audit_target_list <- read_csv("audit_data/ps_audit_target_list.csv")
 
 prelim_ps_reporting_status <- read_csv("validity_checks/prelim_ps_reporting_status.csv")
@@ -206,29 +274,41 @@ ps_summary_report <- read_csv("analysis/ps_summary_report.csv")
 # invalidated_in_prelim <- prelim_af_candidate_ps_data_2019_lite[prelim_af_candidate_ps_data_2019_lite$ps_code %in% invalidated_ps$ps_code, ]
 # all invalidated polling stations in preliminary results report zero votes (not all of zero vote decision stations appear in results)
 
-closed_ps <- invalidated_ps$ps_code[invalidated_ps$commission_decision_or_status == "بسته"]
+closed_ps <- invalidated_ps_data$ps_code[invalidated_ps_data$commission_decision_or_status == "بسته" |
+                                      invalidated_ps_data$commission_decision_or_status == "بسته فیصله 117 بند 3"]
 closed_ps <- tibble(ps_code = closed_ps, ps_open = "NO")
+
+invalidated_ps_data <- invalidated_ps_data %>% 
+  dplyr::select(-ps_open) %>%
+  left_join(closed_ps)
+
+invalidated_ps_data$ps_open[is.na(invalidated_ps_data$ps_open)] <- "YES"
 
 ps_status_update <- prelim_ps_reporting_status %>% 
   dplyr::select(province_code, pc_code, ps_code, ps_type, audit_or_recount, prelim_results_reporting) %>%
   left_join(closed_ps)
 
-invalidated_ps_list <- invalidated_ps %>% filter(commission_decision_or_status != "بسته") %>%
+invalidated_ps_list <- invalidated_ps_data %>% filter(commission_decision_or_status != "بسته" & 
+                                                   commission_decision_or_status != "بسته فیصله 117 بند 3") %>%
   dplyr::select(ps_code) %>% mutate(ps_invalidated = "YES")
 
 ps_status_update <- ps_status_update %>% left_join(invalidated_ps_list)
-ps_status_update$ps_open[ps_status_update$prelim_results_reporting == "YES"] <- "YES"
-ps_status_update$ps_open[ps_status_update$ps_invalidated == "YES"] <- "YES"
 ps_status_update$ps_invalidated[ps_status_update$audit_or_recount == "YES" & is.na(ps_status_update$ps_invalidated)] <- "NO"
+
+ps_status_update$ps_open[ps_status_update$prelim_results_reporting == "YES" & is.na(ps_status_update$ps_open)] <- "YES"
+ps_status_update$ps_open[ps_status_update$ps_invalidated == "YES" & is.na(ps_status_update$ps_open)] <- "YES"
+ps_status_update$ps_open[is.na(ps_status_update$ps_open)] <- "UNKNOWN"
+
+
 ps_status_update <- ps_status_update %>% 
   dplyr::select(province_code, pc_code, ps_code, ps_type, audit_or_recount, prelim_results_reporting,
                 ps_invalidated, ps_open)
-ps_status_update$reporting_zero_votes <- NA
-ps_status_update$reporting_zero_votes[ps_status_update$prelim_results_reporting == "YES" & ps_status_update$ps_invalidated == "YES"] <- "YES"
-ps_status_update$reporting_zero_votes[ps_status_update$prelim_results_reporting == "YES" & is.na(ps_status_update$reporting_zero_votes)] <- "NO"
-ps_status_update$ps_open[is.na(ps_status_update$ps_open)] <- "UNKNOWN"
+
+reporting_zero_votes <- prelim_af_candidate_ps_data_2019_lite %>% group_by(ps_code) %>%
+  summarize(reporting_zero_votes = ifelse(sum(votes) == 0, "YES", "NO"))
 
 ps_status_update <- ps_status_update %>%
+  left_join(reporting_zero_votes) %>%
   dplyr::select(province_code, pc_code, ps_code, ps_type, audit_or_recount, prelim_results_reporting,
                 ps_invalidated, reporting_zero_votes, ps_open)
 
@@ -239,21 +319,25 @@ write.csv(ps_status_update, "./validity_checks/prelim_ps_reporting_status.csv", 
 
 write.csv(missing_open_status, "./validity_checks/prelim_ps_missing_status.csv", row.names = F)
 
-ps_summary_report_update <- ps_summary_report %>% left_join(ps_status_update) %>%
-  rename(pre_audit_decision = commission_decision) %>% left_join((invalidated_ps_data %>% rename(invalidated_results_sheet_total = votes))) %>%
-  rename(post_audit_decision = commission_decision_or_status) %>%
-  dplyr::select(1:9, ps_open, 
-                #accompanying_ps_scan, 
-                audit_or_recount, ps_invalidated, pre_audit_decision, post_audit_decision, 10:17, invalidated_results_sheet_total,
-                everything()) %>%
-  dplyr::select(-c(serial_number, ps_number))
+ps_summary_report_update <- ps_summary_report %>% 
+  dplyr::select(-c(ps_invalidated, ps_open, audit_or_recount, prelim_results_reporting, 
+                   reporting_zero_votes, post_audit_decision, invalidated_results_sheet_total)) %>%
+  left_join(ps_status_update) %>%
+  left_join(dplyr::select(invalidated_ps_data, ps_code, votes, commission_decision_or_status)) %>%
+  rename(post_audit_decision = commission_decision_or_status,
+         invalidated_results_sheet_total = votes
+         )
 
 ps_summary_report_update$post_audit_decision[is.na(ps_summary_report_update$post_audit_decision) & ps_summary_report_update$ps_invalidated == "NO"] <- "Not invalidated in audit or recount"
+ps_summary_report_update$missing_bvv_reporting[is.na(ps_summary_report_update$missing_bvv_reporting)] <- "NO"
 
-ps_summary_report$added_votes[ps_summary_report$prelim_results_reporting == "NO" & ps_summary_report$invalidated_results_sheet_total == 0] <- "NO"
-ps_summary_report$added_votes[ps_summary_report$prelim_results_reporting == "YES" & ps_summary_report$invalidated_results_sheet_total == 0] <- "NO"
-ps_summary_report$added_votes[ps_summary_report$ps_invalidated == "YES"] <- "NO"
-ps_summary_report$added_votes[ps_summary_report$added_votes == "UNKNOWN" & ps_summary_report$total_votes == 0] <- "NO"
-ps_summary_report$added_votes[ps_summary_report$added_votes == "UNKNOWN" & ps_summary_report$prelim_results_reporting == "NO" & ps_summary_report$audit_or_recount == "YES"] <- "NO"
+ps_summary_report_update <- ps_summary_report_update %>%
+  mutate(
+    added_votes = ifelse(pre_post_audit_difference > 0, "YES", "NO")
+  )
+
+ps_summary_report_update <- ps_summary_report_update %>% 
+  dplyr::select(1:9, 33, 32, 36, 34, 10, 38, 11:12, 37, everything()) %>%
+  arrange(province_code, pc_code, ps_code)
 
 write.csv(ps_summary_report_update, "./analysis/ps_summary_report.csv", row.names = F)
